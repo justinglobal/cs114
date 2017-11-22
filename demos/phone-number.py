4b6be5576205a77d87f20a227c783f9d588b757d
justin <justinglobal@gmail.com>
2017-11-22 10:32:59 -0800

    changes finally

--- demos/phone-number.py
+++ demos/phone-number.py
@@ -6,21 +6,25 @@
 phone = input('Phone number? All digits. ')
 
 # 3. Transform
+
 # Work on the dependent steps first.
-has_area_code = len(phone) < 10
+has_area_code = len(phone) >= 10
 
 # Then slowly build up the output you want.
-if has_area_code:
-    phone_parts = [phone[:4], phone[4:7], phone[7:]]
+if has_area_code == True:
+    phone_parts = [phone[:3], phone[3:6], phone[6:]]
+    print(phone_parts)
 else:
-    phone_parts = [phone[:4], phone[4:]]
+    phone_parts = [phone[:3], phone[3:]]
 
 dashed_phone = '-'.join(phone_parts)
-if has_area_code:
-    fancy_phone = '({}) {}-{}'.format(
-        phone_parts[0], phone_parts[1], phone_parts[2])
+
+if has_area_code == True:
+    fancy_phone = '({}) {}-{}'.format(phone_parts[0], phone_parts[1], phone_parts[2])
 else:
     fancy_phone = dashed_phone
+
+
 
 # 4. Output
 print(dashed_phone)
